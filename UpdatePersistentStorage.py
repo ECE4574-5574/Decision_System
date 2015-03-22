adcd8e2def9d7b411e007b8e16e26f078ed16ed2
prerana3 <prerana3@users.noreply.github.com>
2015-03-22 15:06:56 -0400

    Update UpdatePersistentStorage.py

--- UpdatePersistentStorage.py
+++ UpdatePersistentStorage.py
@@ -1,18 +1,22 @@
-/*Prerana Rane*/
-/*Learned behaviours will be stored in the Persistent Storage in the form of Device States*/
+#Prerana Rane
+#Learned behaviours will be stored in the Persistent Storage in the form of Device States
 
 import sqlite3
 
 conn = sqlite3.connect('device.sqlite3')
-cur = conn.cursor()
+cur = conn.cursor()   #Opens the database connection
 
+#If a New Device is added, add a new row in the table 'Devices' and store the state of the device
 cur.execute('INSERT INTO Devices (DeviceID,State, timestamp) VALUES ( ?, ?, ?)', 
     ( 'Light', 'ON', sysdate ) )
+    
+#If learned behaviour is decided    
+#Update the database and store the state    
 cur.execute('UPDATE Devices (DeviceID,State, timestamp)  VALUES ( ?, ? )', 
     ( 'Light', 'OFF', sysdate ) )
 conn.commit()
 
-print 'Tracks:'
+print 'Devices:'
 cur.execute('SELECT DeviceID,State, timestamp FROM Devices')
 for row in cur :
    print row
