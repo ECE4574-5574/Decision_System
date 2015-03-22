7bfdbd685d9b1e77af95362309da0c13c3de1ff9
jigarpatel312 <jigar@vt.edu>
2015-03-22 16:10:12 -0400

    Create criticalAlgorithm.py

---
+++ criticalAlgorithm.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# Definition of learning algorithm for critical devices such as doors, window and alarms
+# These devices are saety critical and hence must be changed without user permission 
+
+def criticalAlgorithm
+  
+return;
