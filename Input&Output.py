b6a123e8f088b5c80ef0d4e585fb8152a1d94660
grahamcc <grahamcc@vt.edu>
2015-03-22 14:53:04 -0400

    Update Input&Output.py

--- Input&Output.py
+++ Input&Output.py
@@ -1,39 +1,38 @@
-#device state has changed
-    #if device is critical,
-    #do CriticalLearningAlgorithm
-    #else
-    #
-def deviceChanged(device, state):
-    if isCritical(device):
-        criticalAlgorithm(device)
-    else:
-        generalAlgorithm(device)
-
-# Checks if device type is in list of critical devices
-def isCritical(device):
-    pass
-
-# Does specific behaviors for device types that function
-# in sepecific ways (e.g. thermostats) or need oversight (e.g. door locks)
-def criticalAlgorithm(device, state):
-    pass
-# Runs general learning algorithm
-def generalAlgorithm(device, state):
-    pass
-
-#Current room user is in        
-def userLocationChanged(user, location):
-    pass
-
-#When an outside stimilus changes (e.g. weather)
-def eventOccurred(event, value):
-    pass
-
-#get learned behavior from previous room details given by user
-def suggestedRoomChanges(room):
-    pass
-
-#get learned behavior from previous time blocks 
-def suggestedTimeChanges(TimeOfDay):
-    pass
+class DecisionSystem:
+    def __init__(self):
+        self.stateList = []
+    # Called when a device state change notification is recieved
+    def deviceChanged(self, device, state):
+        if isCritical(device):
+            criticalAlgorithm(device)
+        else:
+            generalAlgorithm(device)
+    
+    # Checks if device type is in list of critical devices
+    def isCritical(self, device):
+        pass
+    
+    # Does specific behaviors for device types that function
+    # in sepecific ways (e.g. thermostats) or need oversight (e.g. door locks)
+    def criticalAlgorithm(self, device, state):
+        pass
+    # Runs general learning algorithm
+    def generalAlgorithm(self, device, state):
+        pass
+    
+    #Current room user is in        
+    def userLocationChanged(self, user, location):
+        pass
+    
+    #When an outside stimilus changes (e.g. weather)
+    def eventOccurred(self, event, value):
+        pass
+    
+    #get learned behavior from previous room details given by user
+    def suggestedRoomChanges(self, room):
+        pass
+    
+    #get learned behavior from previous time blocks 
+    def suggestedTimeChanges(self, TimeOfDay):
+        pass
 
