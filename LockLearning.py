fa5123c1c1362555f39bc5393833ad16562a7a9d
trevon1 <trevon1@vt.edu>
2015-03-22 20:16:11 -0400

    Update LockLearning.py

--- LockLearning.py
+++ LockLearning.py
@@ -1,13 +1,10 @@
 class Locks:
 def __init__(self):
         self.stateList = [] 
-		
-		
 	#checks to verify the device ID refers to a specific lock
 	#Checks to see if the door is locked or unlocked from Cache
 	#return status	
     def LockStatus(self,state,device)
-	
 	pass
 	
 	#When doors are unlocked get learned behavior from previous time blocks based on previous information. And lock door based from user
