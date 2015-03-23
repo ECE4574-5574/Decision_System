c985351d0340442ea592cbae3741c2179da56bac
jigarpatel312 <jigar@vt.edu>
2015-03-22 21:15:19 -0400

    Create outgoingAPI.py

---
+++ outgoingAPI.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import restful_webapi
+import os
+
+# This example shows how to use the Requests library with RESTful API
+# This web service stores arbitrary JSON data under integer keys
+# We can use GET/POST/PUT/DELETE HTTP methods to modify the data
+
+# Run a local server that we can use
+restful_webapi.run_server()
+
+# post data from the decision algorithm to the Device API
+
+# Stop the server and quit
+os._exit(0)
