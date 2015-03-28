9507fa767ae639e5df34b74152203384c9630593
prerana3 <prerana3@users.noreply.github.com>
2015-03-28 15:59:57 -0400

    Update serverTest.py

--- server/serverTest.py
+++ server/serverTest.py
@@ -2,15 +2,39 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 import requests
 import json
+import httplib
 
-url = 'http://localhost:8080'
+
+"""
+#url = 'https://25.39.122.197:8080'
+url = 'https://172.31.215.239:8080' #Luke's IP
 
 parameters = {
-'GET': 'HD'
+    'path': 'HD/house47/'
 }
+"""
 
-response = requests.get(url, params=parameters)
+conn = httplib.HTTPConnection('172.31.215.239', 8080)
+conn.request('GET', 'HD/HOUSEID/')
+resp = conn.getresponse();	
+conn.request('GET', 'RD/HOUSEID/ROOMID/')
+resp = conn.getresponse();	
+conn.request('GET', 'HT/HOUSEID/TYPE')
+resp = conn.getresponse();	
+conn.request('GET', 'RT/HOUSEID/ROOMID/TYPE')
+resp = conn.getresponse();	
+data = resp.read()
+conn.request('POST', 'R/house2022/15/atrium/')
+resp1 = conn.getresponse()
 
-data = response.json()
+#data = resp.json()
 
+print resp
+print resp1
 print data
+#resp = requests.get(url, params=parameters)
+
+
+#data = resp.json()
+
+#print data
