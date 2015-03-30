e4621c45b1bc8da745fb7646c681a019d5edc103
prerana3 <prerana3@users.noreply.github.com>
2015-03-30 18:16:45 -0400

    Update PersistentStorageAPI.py

--- phase_2/PersistentStorageAPI.py
+++ phase_2/PersistentStorageAPI.py
@@ -5,67 +5,99 @@
 # Receiving HTTP Responses for GET and POST methods sent to the server.
 # Unable to convert to JSON format since there is no data present. Receiving the error "No JSON object to decode"
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import requests
 import json
 import httplib
-import urllib
 
 #COnnection to the server is established
-conn = httplib.HTTPConnection('localhost', 8080)
+#URL provided by Persistent Storage team 
+conn = httplib.HTTPConnection('54.152.190.217', 8080)
 
 """ INCOMING DATA FROM PERSISTENT STORAGE """
 
 #Method to get all devices in a specific house
-print conn.request('GET','HD/HOUSEID/')
-conn.getresponse();
+print conn.request('GET','HD/HOUSE47/')
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all devices in a specific room (for a certain house)
 print conn.request('GET','RD/HOUSEID/ROOMID/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get devices of a specific type in a specific house
 print conn.request('GET','HT/HOUSEID/TYPE')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get devices of a specific type in a specific room (for a certain house)
 print conn.request('GET','RT/HOUSEID/ROOMID/TYPE/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
-#Methods to user information (accesses information from the user’s profile)
-print conn.request('GET','UI/HOUSEID')
-conn.getresponse();
-print conn.request('GET','UI/USERID')
-conn.getresponse();
+#Methods to user information (accesses information from the users profile)
+print conn.request('GET','UI/HOUSEID/')
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
+print conn.request('GET','UI/USERID/')
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all log entries (actions) for a given location
 print conn.request('GET','AL/USERID/TIMEFRAME/HOUSEID*/ROOMID*/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all log entries (actions) by device type
 print conn.request('GET','AT/USERID/TIMEFRAME/DEVICETYPE/HOUSEID/ROOMID/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all log entries (actions) by device ID
 print conn.request('GET','AI/USERID/TIMEFRAME/DEVICEID/HOUSEID/ROOMID/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all computer (predictive algorithm) log entries by location
 print conn.request('GET','CL/USERID/TIMEFRAME/HOUSEID/ROOMID/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all computer (predictive algorithm) log entries by type
 print conn.request('GET','CT/USERID/TIMEFRAME/DEVICETYPE/HOUSEID/ROOMID/')
-conn.getresponse();
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 #Method to get all computer (predictive algorithm) log entries by device ID
 print conn.request('GET','CI/USERID/TIMEFRAME/DEVICEID/HOUSEID/ROOMID/')
-conn.getresponse();
-
+r1 = conn.getresponse();
+print r1.status, r1.reason
+d1 = r1.read()
+print json.loads(d1)
 
 """ PROCESSING OF DATA """
-
-
 
 """ OUTGOING CALL TO PERSISTENT STORAGE """
 
@@ -95,15 +127,4 @@ print data
 
 data1 = json.loads(data)
 print data1
-
-
-#import httplib
-conn = httplib.HTTPConnection("www.python.org")
-conn.request("GET", "/index.html")
-r1 = conn.getresponse()
-print r1.status, r1.reason
-data1 = r1.read()
-
-print data1
-#conn.close()
 """
