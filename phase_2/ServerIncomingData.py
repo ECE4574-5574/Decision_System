7bee3b5b2477bf32f11f30ca5635c694e6280c94
Andrew <gandrew2@vt.edu>
2015-04-06 18:50:45 -0400

    Format location time and send location to storage

--- phase_2/ServerIncomingData.py
+++ phase_2/ServerIncomingData.py
@@ -2,8 +2,11 @@ import BaseHTTPServer
 import SocketServer
 import json
 import decisions
+import httplib
+from temporaryHolding import TemporaryHolding
+from datetime import datetime
 
-class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler): 
   def do_POST(self):
     length = int(self.headers.getheader('content-length', 0))
     data = self.rfile.read(length)
@@ -11,7 +14,7 @@ class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     if self.path == "/Weather":
       print "The weather condition is " + str(message["condition"])
       print "The temperature is " + str(message["temperature"])
-      print "Timestamp of WeatherUpdate " + str(message["WeatherTimeStamp"])
+      print "Timestamp of WeatherUpdate " + str(message["time"])
       decisions.randomDecision()
       self.send_response(200)
     elif self.path == "/DeviceState":
@@ -20,15 +23,24 @@ class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         print "The Device Type is " + str(message["deviceType"])
         print "The SpaceID is " + str(message["spaceID"])
         print "The state is " + str(message["stateDevice"])
-        print "Timestamp of DeviceState Action " + str(message["DeviceStateTimeStamp"])
+        print "Timestamp of DeviceState Action " + str(message["time"])
         decisions.randomDecision()
         self.send_response(200)
     elif self.path == "/LocationChange":
-        print "The UserID that changed location is " + str(message["usersID"])
-        print "The Latitude is " + str(message["latitude"])
-        print "The Longitude is  " + str(message["longitude"])
-        print "The Altitude is " + str(message["altitude"])
-        print "Timestamp of LocationChange " + str(message["locationTimeStamp"])
+        temp = TemporaryHolding()
+        print "The Latitude is " + str(message["lat"])
+        print "The Longitude is  " + str(message["long"])
+        print "The Altitude is " + str(message["alt"])
+        conn = httplib.HTTPConnection("54.152.190.217", 8081)
+        dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
+        formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")
+        print formatted
+        payload = json.dumps({"action-type":"location-update","action-data":message})
+        conn.request('PATCH', 'A/' + 'bsaget' + '/' + formatted + '/' + 'WayneManor', payload) 
+        response = conn.getresponse()
+        print response.status
+        print response.read()     
+        print "Timestamp of LocationChange " + str(message["time"])
         decisions.randomDecision()
         self.send_response(200)
     elif self.path == "/CommandsFromApp":
@@ -38,7 +50,7 @@ class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         print "The Device Type is " + str(message["commanddeviceType"])
         print "The SpaceID is " + str(message["commandspaceID"])
         print "The state is " + str(message["commandstateDevice"])
-        print "Timestamp of Command " + str(message["CommandTimeStamp"])
+        print "Timestamp of Command " + str(message["time"])
         decisions.randomDecision()
         self.send_response(200)
     elif self.path == "/LocalTime":
--- phase_2/ServerIncomingDataTest.py
+++ phase_2/ServerIncomingDataTest.py
@@ -6,22 +6,22 @@ connection = httplib.HTTPConnection('localhost', 8081)
 
 
 
-weather = {"condition":"Sunny", "temperature":70, "WeatherTimeStamp":str(datetime.datetime.now())}
-deviceState = {"deviceID":1, "deviceName":"BedroomLight", "deviceType":3, "spaceID": 4, "stateDevice":1, "DeviceStateTimeStamp":str(datetime.datetime.now())}
-locationChange = {"usersID":3, "latitude":70, "longitude":300, "altitude":150, "locationTimeStamp":str(datetime.datetime.now())}
-commandsfromApp = {"commandUserID":4,"commanddeviceID":1, "commanddeviceName":"BedroomLight", "commanddeviceType":3, "commandspaceID": 4, "commandstateDevice":0, "CommandTimeStamp":str(datetime.datetime.now())}
+weather = {"condition":"Sunny", "temperature":70, "time":str(datetime.datetime.now())}
+deviceState = {"deviceID":1, "deviceName":"BedroomLight", "deviceType":3, "spaceID": 4, "stateDevice":1, "time":str(datetime.datetime.now())}
+locationChange = {"lat":70.123456, "long":300.123456, "alt":150.123456, "time":"2015-04-06 18:05:05"}
+commandsfromApp = {"commandUserID":4,"commanddeviceID":1, "commanddeviceName":"BedroomLight", "commanddeviceType":3, "commandspaceID": 4, "commandstateDevice":0, "time":str(datetime.datetime.now())}
 time = {"localTime":str(datetime.datetime.now())}
 
 
 connection.request('POST', '/Weather', json.dumps(weather))
-res =  connection.getresponse()
+res = connection.getresponse()
 connection.request('POST', '/DeviceState', json.dumps(deviceState))
-res =  connection.getresponse()
+res = connection.getresponse()
 connection.request('POST', '/LocationChange', json.dumps(locationChange))
-res =  connection.getresponse()
+res = connection.getresponse()
 connection.request('POST', '/CommandsFromApp', json.dumps(commandsfromApp))
-res =  connection.getresponse()
+res = connection.getresponse()
 connection.request('POST', '/LocalTime', json.dumps(time))
-res =  connection.getresponse()
+res = connection.getresponse()
 connection.request('POST', '/CausesError', json.dumps(time))
 res = connection.getresponse()
