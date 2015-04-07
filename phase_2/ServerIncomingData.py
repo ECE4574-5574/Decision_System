166c4bd1ce75522129d96243c13ce61e9ecaabec
Andrew <gandrew2@vt.edu>
2015-04-06 20:49:55 -0400

    started commenting

--- phase_2/ServerIncomingData.py
+++ phase_2/ServerIncomingData.py
@@ -6,17 +6,21 @@ import httplib
 from temporaryHolding import TemporaryHolding
 from datetime import datetime
 
-class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler): 
+#Class that gets called whenever there is an HTTP request
+class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   def do_POST(self):
     length = int(self.headers.getheader('content-length', 0))
     data = self.rfile.read(length)
+    #Dictionary of the json string received
     message = json.loads(data)
+    #If a weather update is received then that information is printed and a 200 response is sent
     if self.path == "/Weather":
-      print "The weather condition is " + str(message["condition"])
-      print "The temperature is " + str(message["temperature"])
-      print "Timestamp of WeatherUpdate " + str(message["time"])
-      decisions.randomDecision()
-      self.send_response(200)
+        print "The weather condition is " + str(message["condition"])
+        print "The temperature is " + str(message["temperature"])
+        print "Timestamp of WeatherUpdate " + str(message["time"])
+        decisions.randomDecision()
+        self.send_response(200)
+    #If a device state update is received then that inofrmation is printed and a 200 response is sent
     elif self.path == "/DeviceState":
         print "The Device ID is " + str(message["deviceID"])
         print "The Device Name is " + str(message["deviceName"])
@@ -24,24 +28,27 @@ class ServerInfoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         print "The SpaceID is " + str(message["spaceID"])
         print "The state is " + str(message["stateDevice"])
         print "Timestamp of DeviceState Action " + str(message["time"])
-        decisions.randomDecision()
         self.send_response(200)
+    #If a location change update is received the information is printed and sent to persistent storage. A decision is also made based on the change of location
+    #and a 200 response is sent
     elif self.path == "/LocationChange":
         temp = TemporaryHolding()
         print "The Latitude is " + str(message["lat"])
         print "The Longitude is  " + str(message["long"])
         print "The Altitude is " + str(message["alt"])
+        print "Timestamp of LocationChange " + str(message["time"])
+        #Set up connection to persistent storage
         conn = httplib.HTTPConnection("54.152.190.217", 8081)
+        #change the format to the format required by persistent storage
         dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
         formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")
-        print formatted
+        #P
         payload = json.dumps({"action-type":"location-update","action-data":message})
-        conn.request('PATCH', 'A/' + 'bsaget' + '/' + formatted + '/' + 'WayneManor', payload) 
+        conn.request('PATCH', 'A/' + 'bsaget' + '/' + formatted + '/' + 'WayneManor', payload)
         response = conn.getresponse()
         print response.status
-        print response.read()     
-        print "Timestamp of LocationChange " + str(message["time"])
-        decisions.randomDecision()
+        print response.read()
+        decisions.randomDecision(float(lat), float(longi), float(alt), formatted)        
         self.send_response(200)
     elif self.path == "/CommandsFromApp":
         print "For User " + str(message["commandUserID"])
