c4cb4ba66480b0c27968458c982ca058465a19ac
Mark Koninckx <koninckx@vt.edu>
2015-04-15 22:17:18 -0400

    Add something to start fleshing out the third-party test script

--- phase_2/ServerIncomingDataTest.py
+++ phase_2/ServerIncomingDataTest.py
@@ -1,29 +1,47 @@
 import httplib
 import json
 import datetime
+import argparse
 
-connection = httplib.HTTPConnection('localhost', 8081)
+parser = argparse.ArgumentParser(description='Get port number...')
+parser.add_argument('-p', '--port', type=int)
+args=parser.parse_args()
+
+connection = httplib.HTTPConnection('localhost', args.port)
 
 
-
-weather = {"condition":"Sunny", "temperature":70, "time":str(datetime.datetime.now())}
-deviceState = {"deviceID":1, "deviceName":"BedroomLight", "deviceType":3, "spaceID": 4, "stateDevice":1, "time":str(datetime.datetime.now())}
+deviceState = {"deviceName":"BedroomLight", "deviceType":3, "enabled":"true", "setpoint:":5, "time":"2015-04-06 18:05:05"}
 locationChange = {"userId":"user1", "lat":70.123456, "long":300.123456, "alt":150.123456, "time":"2015-04-06 18:05:05"}
-commandsfromApp = {"commandUserID":4,"commanddeviceID":1, "commanddeviceName":"BedroomLight", "commanddeviceType":3, "commandspaceID": 4, "commandstateDevice":0, "time":str(datetime.datetime.now())}
 time = {"localTime":str(datetime.datetime.now())}
 
-
-connection.request('POST', '/Weather', json.dumps(weather))
-res = connection.getresponse()
+#Test device state change response.
+print 'Testing response to device state change':
 connection.request('POST', '/DeviceState', json.dumps(deviceState))
 res = connection.getresponse()
+print 'Device State Change Response' + str(res.status)
+if (res.status == 200):
+    print 'PASS'
+else
+    print 'FAIL'
+
+#Test location change response.
+print 'Testing response to location change...'
+print 'POST/LocationChange ' + json.dumps(locationChange)
 connection.request('POST', '/LocationChange', json.dumps(locationChange))
 res = connection.getresponse()
 print 'Location Change Response' + str(res.status)
-print 'Location Change Body:\n' + str(res.read())
+if (res.status == 200):
+    print 'PASS'
+else
+    print 'FAIL'
+    
+#Test 
+print 'Testing response to app command...'
 connection.request('POST', '/CommandsFromApp', json.dumps(commandsfromApp))
 res = connection.getresponse()
+
 connection.request('POST', '/LocalTime', json.dumps(time))
 res = connection.getresponse()
+
 connection.request('POST', '/CausesError', json.dumps(time))
 res = connection.getresponse()
