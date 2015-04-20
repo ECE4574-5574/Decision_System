861734bd3140a817ec8b13106589eaba6fe23325
Andrew <gandrew2@vt.edu>
2015-04-20 17:37:39 -0400

    added error handling to the decision class

--- phase_2/decisionClass.py
+++ phase_2/decisionClass.py
@@ -12,7 +12,6 @@ import time
 import httplib
 import argparse
 import sys
-from temporaryHolding import TemporaryHolding
 from datetime import datetime
 import decisions
 import logging
@@ -31,50 +30,65 @@ class decisionMaking():
 
     #message should be a parsed JSON dictionary
     def weatherDecision(self, message):
-        line = "Weather Decision " + str(self.weatherDecisionCount) + ":\n"
-        self.weatherDecisionCount += 1
-        self.logger.debug(line)
-        
+        try:    
+            line = "Weather Decision " + str(self.weatherDecisionCount) + ":\n"
+            self.weatherDecisionCount += 1
+            self.logger.debug(line)
+        except:
+            line = 'Error when trying to make a weather decision!\nRequest body being handled:\n' + str(message) + '\n'
+            self.logger.warning(line)
 
     def deviceStateDecision(self, message):
-        line = "Device State Decision " + str(self.deviceStateDecisionCount) + ":\n"
-        self.deviceStateDecisionCount += 1
-        self.logger.debug(line)
-        # Begin - Prerana Rane 4/15/2015
-        #Logging the device state changes in the persistent storage 
-        #Set up connection to persistent storage
-        conn = httplib.HTTPConnection(self.storageAddress[0], self.storageAddress[1])
-        #change the format to the format required by persistent storage
-        dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
-        formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")
-        payload = json.dumps({"action-type":"device state change","action-data":message})
-        conn.request('PATCH', 'C/' + "user1" + '/' + formatted + '/' + 'WayneManor' + '/' + "aspace" + '/' + message['deviceName'], payload)
-        response = conn.getresponse()
-        print response.status
-        print response.read()               
-        #End - Prerana Rane 4/15/2015
+        try:
+            line = "Device State Decision " + str(self.deviceStateDecisionCount) + ":\n"
+            self.deviceStateDecisionCount += 1
+            self.logger.debug(line)
+            # Begin - Prerana Rane 4/15/2015
+            #Logging the device state changes in the persistent storage 
+            #Set up connection to persistent storage
+            conn = httplib.HTTPConnection(self.storageAddress[0], self.storageAddress[1])
+            #change the format to the format required by persistent storage
+            dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
+            formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")
+            payload = json.dumps({"action-type":"device state change","action-data":message})
+            conn.request('PATCH', 'C/' + "user1" + '/' + formatted + '/' + 'WayneManor' + '/' + "aspace" + '/' + message['deviceName'], payload)
+            response = conn.getresponse()
+            print response.status
+            print response.read()               
+            #End - Prerana Rane 4/15/2015
+        except:
+            line = 'Error when trying to make a device state decision!\nRequest body being handled:\n' + str(message) + '\n'
+            self.logger.warning(line)
 
     def locationDecision(self, message):
-        #change the format to the format required by persistent storage
-        dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
-        formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")        
-        line = "Location Decision " + str(self.locationDecisionCount) + ":\n"
-        self.locationDecisionCount += 1
-        self.logger.debug(line)
-        #Set up connection to persistent storage
-        conn = httplib.HTTPConnection(self.storageAddress[0],self.storageAddress[1])
-        #Pass the JSON string to persistent storage
-        payload = json.dumps({"action-type":"location-update","action-data":message})
-        conn.request('PATCH', 'A/' + message['userId'] + '/' + formatted + '/' + 'WayneManor', payload)
-        response = conn.getresponse()
-        print response.status
-        print response.read()
-        #make a random decision
+        try:    
+            #change the format to the format required by persistent storage
+            dateTimeObject = datetime.strptime(message["time"], "%Y-%m-%d %H:%M:%S")
+            formatted = dateTimeObject.strftime("%Y-%m-%dT%H:%M:%SZ")        
+            line = "Location Decision " + str(self.locationDecisionCount) + ":\n"
+            self.locationDecisionCount += 1
+            self.logger.debug(line)
+            #Set up connection to persistent storage
+            conn = httplib.HTTPConnection(self.storageAddress[0],self.storageAddress[1])
+            #Pass the JSON string to persistent storage
+            payload = json.dumps({"action-type":"location-update","action-data":message})
+            conn.request('PATCH', 'A/' + message['userId'] + '/' + formatted + '/' + 'WayneManor', payload)
+            response = conn.getresponse()
+            print response.status
+            print response.read()
+            #make a random decision
+        except:
+            line = 'Error when trying to make a location decision!\nRequest body being handled:\n' + str(message) + '\n'
+            self.logger.warning(line)
 
     def timeDecision(self, message):
-        line = "Time Decision " + str(self.timeDecisionCount) + ":\n"
-        self.timeDecisionCount += 1
-        self.logger.debug(line)
+        try:
+            line = "Time Decision " + str(self.timeDecisionCount) + ":\n"
+            self.timeDecisionCount += 1
+            self.logger.debug(line)
+        except:
+            line = 'Error when trying to make a time decision!\nRequest body being handled:\n' + str(message) + '\n'
+            self.logger.warning(line)
 
     def command(self, message):
         line = "Command " + str(self.commandCount) + ":\n"
