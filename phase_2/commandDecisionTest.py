6050d49e3f6611822bc1aed779c0c4cc65b7f806
Mark Koninckx <koninckx@vt.edu>
2015-04-20 12:02:20 -0400

    Convert to use unittest framework (even though these aren't strictly speaking unit tests.

--- phase_2/commandDecisionTest.py
+++ phase_2/commandDecisionTest.py
@@ -2,6 +2,7 @@
 
 import json
 import httplib
+import unittest
 from decisionClass import decisionMaking
 
 def checkStatus(expected, response):
@@ -26,51 +27,56 @@ def validateLog(expected, actual):
             return False
     return True
 
-if __name__ == "__main__":
-    APP_COMMAND_PATH = '/CommandsFromApp'
-    OUTPUT_FILE = 'commandtestlog.txt'
-    testoutfile = open(OUTPUT_FILE, 'w')
     
-    ACTUAL_USER_ID = 'bsaget'
-    ACTUAL_HOUSE_LAT = 37.23512
-    ACTUAL_HOUSE_LON = -80.41352
-    ACTUAL_HOUSE_ALT = 100
-    ACTUAL_COMMAND_STRING = 'brightenNearMe'
+OUTPUT_FILE = 'commandtestlog.txt'
+
+ACTUAL_USER_ID = 'bsaget'
+ACTUAL_HOUSE_LAT = 37.23512
+ACTUAL_HOUSE_LON = -80.41352
+ACTUAL_HOUSE_ALT = 100
+ACTUAL_COMMAND_STRING = 'brightenNearMe'
+
+good_request_no_user = {'userID': 'nouser',
+                        'lat': ACTUAL_HOUSE_LAT,
+                        'lon': ACTUAL_HOUSE_LON,
+                        'alt': ACTUAL_HOUSE_ALT,
+                        'command-string':ACTUAL_COMMAND_STRING}
+good_request_no_house = {'userID': ACTUAL_USER_ID,
+                         'lat': 0, 'lon': 0, 'alt': 0,
+                         'command-string':ACTUAL_COMMAND_STRING}
+good_request_should_work = {'userID': ACTUAL_USER_ID,
+                        'lat': ACTUAL_HOUSE_LAT,
+                        'lon': ACTUAL_HOUSE_LON,
+                        'alt': ACTUAL_HOUSE_ALT,
+                        'command-string':ACTUAL_COMMAND_STRING}
+class commandDecisionTest(unittest.TestCase):
     
-    good_request_no_user = {'userID': 'nouser',
-                            'lat': ACTUAL_HOUSE_LAT,
-                            'lon': ACTUAL_HOUSE_LON,
-                            'alt': ACTUAL_HOUSE_ALT,
-                            'command-string':ACTUAL_COMMAND_STRING}
-    good_request_no_house = {'userID': ACTUAL_USER_ID,
-                             'lat': 0, 'lon': 0, 'alt': 0,
-                             'command-string':ACTUAL_COMMAND_STRING}
-    good_request_should_work = {'userID': ACTUAL_USER_ID,
-                            'lat': ACTUAL_HOUSE_LAT,
-                            'lon': ACTUAL_HOUSE_LON,
-                            'alt': ACTUAL_HOUSE_ALT,
-                            'command-string':ACTUAL_COMMAND_STRING}
-    
-    #For right now (i.e. Sunday), only good_request_no_user should work, since the data we're looking for won't be in the database.
-    
-    
-    #First, we check the behavior of the decision class directly.
+    #We check the behavior of the decision class directly.
     #We do this because when the server is actually running, the decision runs asynchronously,
-    #so it is a little bit harder to test directly.
-    print 'Testing command decision when user does not exist.'
-    dmaking = decisionMaking(testoutfile, ['localhost', 8080], 'dummy')
-    dmaking.command(good_request_no_user, 0)
-    testoutfile.close()
-    testoutfile = open(OUTPUT_FILE)
-    log = testoutfile.read()
-    expected = ['req localhost:8080 GET UI/nouser',
-                'response 404']
-    if validateLog(expected, log):
-        print 'OK!'
-    else:
-        print 'FAIL'
-    
-    decision_server_port = 8085
+    #so it is a little bit harder to test.
+    def testNoMatchingUser(self):
+        testoutfile = open(OUTPUT_FILE, 'w')
+        print 'Testing command decision when user does not exist.'
+        dmaking = decisionMaking(testoutfile, ['localhost', 8080], 'dummy')
+        dmaking.command(good_request_no_user, 0)
+        testoutfile.close()
+        testoutfile = open(OUTPUT_FILE)
+        log = testoutfile.read()
+        expected = ['req localhost:8080 GET UI/nouser',
+                    'response 404']
+        self.assertTrue(validateLog(expected, log))
+
+notJson = "this is some garbage that isn't json"
+
+missingKeys = json.dumps({'userID':'some-user-id',
+    'lat':0.1111, 'lon':0.1111, 
+    'command-string':'brightenNearMe'})
+notANumber = json.dumps({'userID':'some-user-id',
+    'lat':0.1111, 'lon':'garbage', 'alt':0.111,
+    'command-string':'brightenNearMe'})
+decision_server_port = 8085
+APP_COMMAND_PATH = '/CommandsFromApp'
+class serverResponseTest(unittest.TestCase):
     #Then, let's check the server's ability to sort through bad input.
     #We expect a JSON object with the following schema:
     #{
@@ -81,37 +87,30 @@ if __name__ == "__main__":
     #    "command-string":"brightenNearMe"
     #}
     #Anything else should be gracefully rejected.
-
-    notJson = "this is some garbage that isn't json"
-
-    missingKeys = json.dumps({'userID':'some-user-id',
-        'lat':0.1111, 'lon':0.1111, 
-        'command-string':'brightenNearMe'})
-
-    notANumber = json.dumps({'userID':'some-user-id',
-        'lat':0.1111, 'lon':'garbage', 'alt':0.111,
-        'command-string':'brightenNearMe'})
+    
+    def setUp(self):
+        self.conn = httplib.HTTPConnection('localhost', decision_server_port)
+    
+    def testNonJSON(self):
+        print 'Checking response to POST with non-json body...'
+        self.conn.request('POST', APP_COMMAND_PATH, notJson)
+        self.assertEqual(400, self.conn.getresponse().status)
+     
+    def testMissingFields(self):
+        print 'Checking response to POST with missing JSON fields...'
+        self.conn.request('POST', APP_COMMAND_PATH, missingKeys)
+        self.assertEqual(400, self.conn.getresponse().status)
+    
+    def testBadCoords(self):
+        print 'Checking response to POST with bad data in JSON fields...'
+        self.conn.request('POST', APP_COMMAND_PATH, notANumber)
+        self.assertEqual(400, self.conn.getresponse().status)
+    
+    def testGoodRequ(self):
+        print 'Checking response to POST with good request with no side effects...'
+        self.conn.request('POST', APP_COMMAND_PATH, json.dumps(good_request_no_user))
+        self.assertEqual(200, self.conn.getresponse().status)
+    
         
-    conn = httplib.HTTPConnection('localhost', decision_server_port)
-    
-    all_OK = True    
-    print 'Checking response to POST with non-json body...'
-    conn.request('POST', APP_COMMAND_PATH, notJson)
-    all_OK = all_OK and checkStatus(400, conn.getresponse())
-    
-    print 'Checking response to POST with missing JSON fields...'
-    conn.request('POST', APP_COMMAND_PATH, missingKeys)
-    all_OK = all_OK and checkStatus(400, conn.getresponse())
-    
-    print 'Checking response to POST with bad data in JSON fields...'
-    conn.request('POST', APP_COMMAND_PATH, notANumber)
-    all_OK = all_OK and checkStatus(400, conn.getresponse())
-    
-    print 'Checking response to POST with good request with no side effects...'
-    conn.request('POST', APP_COMMAND_PATH, json.dumps(good_request_no_user))
-    all_OK = all_OK and checkStatus(200, conn.getresponse())
-    
-    if all_OK:
-        print 'Server responses are OK'
-    else:
-        print 'TEST FAILED.'
+if __name__ == "__main__":
+    unittest.main()
