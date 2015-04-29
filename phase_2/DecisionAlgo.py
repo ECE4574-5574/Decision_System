39749e1ba982715f78fe3f2cab0438dbff8b8cf2
jigarpatel312 <jigar@vt.edu>
2015-04-29 07:08:54 -0400

    Update DecisionAlgo.py

--- phase_2/DecisionAlgo.py
+++ phase_2/DecisionAlgo.py
@@ -1,6 +1,6 @@
 """Author: Jigar Patel
 Date : 4/29/205
-Purpose: Generates a decision based on the location of the users."""
+Purpose: Generates a decision based on the HISTORY of the ROOM."""
 
 import requests
 import json
@@ -8,7 +8,7 @@ import os
 import httplib
 
 
-def randomDecision(lat, longi, alt, userId, timeframe, persistent, server, logger):   
+def Decision(lat, longi, alt, userId, timeframe, persistent, server, logger):   
     # Run the URL hosted by server API teams
     url = server         #URL for Server API
     conn = httplib.HTTPConnection(persistent[0], persistent[1])                       #IP Address for Persistent Storage
@@ -17,9 +17,15 @@ def randomDecision(lat, longi, alt, userId, timeframe, persistent, server, logge
     
 # Check the room user is in at present, ie if the lat and lon map to a room
 
+
+
 # Get the snapshot of devices last time the user was in the room
 
+
+# Change state of the devices based on the last snapshot of the room
+
 # If there are any manual overrides by the user, update the snapshot entry
+
 
 # If user moves to another room, take snapshot of the room and store in persistent storage
 
