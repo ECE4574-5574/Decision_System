04e4383f08646988b9e631ff325ef508eceff6a0
jigarpatel312 <jigar@vt.edu>
2015-04-29 06:57:59 -0400

    Update DecisionAlgo.py

--- phase_2/DecisionAlgo.py
+++ phase_2/DecisionAlgo.py
@@ -1,1 +1,16 @@
+"""Author: Jigar Patel
+Date : 4/29/205
+Purpose: Generates a decision based on the location of the users."""
 
+import requests
+import json
+import os
+import httplib
+
+
+def randomDecision(lat, longi, alt, userId, timeframe, persistent, server, logger):   
+    # Run the URL hosted by server API teams
+    url = server         #URL for Server API
+    conn = httplib.HTTPConnection(persistent[0], persistent[1])                       #IP Address for Persistent Storage
+    # Header for JSON data objects
+    headers = {'Content-type': 'application/json'}
